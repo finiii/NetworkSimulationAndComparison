3f4744003fc62e1f9492864c7b9bf5852b463870
finiii <jojome.stark@gmail.com>
2023-10-27 13:47:20 +0200

    new simulations and convergence checks

--- generate_pPGM.R
+++ generate_pPGM.R
@@ -8,6 +8,8 @@ library("MASS")
 library("igraph")
 library("ggplot2")
 library("dplyr")
+#for the diagnostics
+library(coda)
 
 
 
@@ -27,7 +29,7 @@ theta <- matrix(0, nrow = dim, ncol = dim)
 
 #swet the upper and the lower bound for the uniform distribution the edge weights are drawn from
 lower_bound = -1
-upper_bound = 0
+upper_bound = -0
 
 # Fill the upper triangular part with random negative values
 for (i in 1:(dim - 1)) {
@@ -84,8 +86,8 @@ for (i in 1:dim){
 }
 
 # iterations
-iterations = 5000  #N
-burn_in = 200 # burn in length
+iterations = 10000  #N
+burn_in = iterations/2 # burn in length
 log_likelihood_values = numeric(iterations)
 eta_minus_i_old = matrix(eta_0, nrow = dim, ncol = n_samples)
 eta_minus_i= matrix(NA, nrow = dim, ncol = n_samples)
@@ -99,6 +101,8 @@ for (i in 1:dim){
   name <- paste0("X_", i, "_simulations")
   assign(name, matrix(X_0[i,], nrow = n_samples, ncol = 1))
 }
+
+
 
 #here the gibbs sampling starts
 for(loop in 1:iterations){
@@ -133,38 +137,121 @@ for(loop in 1:iterations){
       name <- paste0("X_", i, "_simulations")
       assign(name, cbind(get(name), X_new[i,]))
     }
+
   }
 
-
+simulation_name <- paste0("pPGM_simulation_dim", dim, "_lower_bound_", lower_bound, "_upper_bound_", upper_bound, "_n_samples_", n_samples, "_iterations_", iterations)
 #save the results
 result = list(X_new = X_new, mean_X_new = mean_X_new)
-save(result, file = paste0("/dss/dsshome1/03/ga27hec2/NetworkSimulationAndComparison/simulations_Poisson/pPGM_simulation_dim", dim, "_lower_bound_", lower_bound, "_upper_bound_", upper_bound, "_n_samples_", n_samples, "_iterations_", iterations,".RData"))
+save(result, file = paste0("/dss/dsshome1/03/ga27hec2/NetworkSimulationAndComparison/simulations_Poisson/", simulation_name,".RData"))
 
-  #delete the samples from the burn in phase
 
-  X_new = X_new[, -c(1:burn_in)]
-  mean_X_new = mean_X_new[, -c(1:burn_in)]
-  mean_per_chain = mean_per_chain[, -c(1:burn_in)]
-  for(i in 1:dim){
+#I took this function from the coda package and changed it a bit
+"gelman.preplot" <-
+  function (x, max.bins = 50, confidence = 0.95, bin.width = 1, autoburnin = TRUE) 
+{
+  x_with_burnin <- x
+  if (autoburnin && start(x) < end(x)/2 ) 
+  x <- window(x, start = end(x)/2 + 1)
+  nbin <- floor((niter(x) - 50)/thin(x))
+  binw <- min(floor((niter(x) - 50)/nbin), bin.width)
+  last.iter <- c(seq(from = start(x) + 50 * thin(x), by = binw * 
+                     thin(x), length = nbin), end(x))
+  #shrink <- array(dim = c(nbin + 1, nvar(x), 2))
+  shrink <- array(dim = c(niter(x_with_burnin), nvar(x), 2))
+  dimnames(shrink) <- list(1:niter(x_with_burnin), varnames(x),
+                           c("median", paste(50 * (confidence + 1), "%",
+                                             sep = ""))
+                           )
+  #the first entries correponding to the burn in should be zero! autobutnin = TRUE cuts half of the samples
+  for (j in 1:(floor((niter(x_with_burnin))/2))){
+      shrink[j, , ] <- NA
+  }
+  for (i in 1:(nbin)) {
+
+    shrink[i+(floor((niter(x_with_burnin))/2)+1)+50, , ] <- gelman.diag(window(x, end = last.iter[i]),
+                                 multivariate = TRUE,
+                                 autoburnin = TRUE)$psrf
+  }
+  return(list(shrink = shrink, last.iter = last.iter))
+}
+
+
+###prepare for R hat with coda
+for (i in 1:dim){
       name <- paste0("X_", i, "_simulations")
       simulated_data <- get(name)
-      assign(name, simulated_data[, -c(1:burn_in)])
+      mcmc.list <- mcmc.list(as.mcmc(simulated_data[1,]), as.mcmc(simulated_data[2,]), as.mcmc(simulated_data[3,]) ,as.mcmc(simulated_data[4,]))
+      new_name <- paste0("mcmc.list.X", i)
+      assign(new_name, mcmc.list)
+}
+
+
+#R hat per iteration and dimension
+for (i in 1:dim){
+  name <- paste0("X_", i, "_Rhat")
+  assign(name, rep(NA, iterations))
+}
+
+
+for (i in 1:dim){
+  name <- paste0("X_", i, "_Rhat")
+    mcmc.list <- paste0("mcmc.list.X", i)
+    mcmc.list <- get(mcmc.list)
+    assign(name, as.vector(gelman.preplot(mcmc.list)$shrink[,1,1]))
+}
+
+
+i = 1
+#build the Rhat convergence check
+#ich glaube hier ist noch irgendwas falsch weil die werte nicht mit den werten aus den plots übereinstimmen!!!
+convergence_check_Rhat <- function(limit = 1.1){
+  non_converged_chains <- list()
+  for (i in 1:dim){
+    name <- paste0("X_", i, "_Rhat")
+    Rhat <- get(name)
+      if (any(Rhat > limit,na.rm = TRUE)){
+        sum_bigger <- sum(Rhat > limit, na.rm = TRUE)
+       message <- paste0("X_", i, " did not converge ", sum_bigger," Rhat values are not smaller than ", limit)
+        non_converged_chains <- c(non_converged_chains, message)
+      }
+    }
+  if (length(non_converged_chains) == 0) {
+    return("All chains converged.")
+  } else {
+    return(paste("Chains that did not converge:", non_converged_chains))
+  }
   }
 
-#hier plotte ich alle running means der variablen über die chains 
-#also x.B. mean_X_new[1,] ist der running mean der ersten variable über die 4 chains
-picture_file  = paste0("/dss/dsshome1/03/ga27hec2/NetworkSimulationAndComparison/simulations_Poisson/pPGM_simulation_dim", dim, "_lower_bound_", lower_bound, "_upper_bound_", upper_bound, "_n_samples_", n_samples, "_iterations_", iterations,".pdf")
+convergence_check_Rhat(limit = 1.1)
 
-pdf(file = picture_file)
-for(i in 1:dim){
-  plot(mean_X_new[i,], type = "l")
+#build the acf convergence check
+convergence_check_acf <- function(burn_in, limit = 0.05){
+  non_converged_chains <- list()
+  for (i in 1:dim){
+    name <- paste0("X_", i, "_simulations")
+    simulated_data <- get(name)
+    for (j in 1:n_samples){
+      acf_values <- acf(simulated_data[j, -c(1:burn_in)], plot = FALSE)$acf
+      if (any(acf_values[length(acf_values)] > limit)){
+              message <- paste0("X_", i, " chain ", j, " did not converge, the last acf value is ", acf_values[length(acf_values)], " and should be smaller than ", limit)
+        non_converged_chains <- c(non_converged_chains, message)
+      }
+    }
+  }
+  
+  if (length(non_converged_chains) == 0) {
+    return("All chains converged.")
+  } else {
+    return(paste("Chains that did not converge:", non_converged_chains))
+  }
 }
-plot(report, type = "l")
-dev.off()
+convergence_check_acf(burn_in)
 
+#diagnostic plots
+pdf(file = paste0("/dss/dsshome1/03/ga27hec2/NetworkSimulationAndComparison/diagnostic_plots/", simulation_name,".pdf"))
 
 #density plots
-pdf(file = "/dss/dsshome1/03/ga27hec2/NetworkSimulationAndComparison/density_plots.pdf")
 for(i in 1:dim){
   name <- paste0("X_", i, "_simulations")
   simulated_data <- as_tibble(get(name))
@@ -178,123 +265,23 @@ for(i in 1:dim){
 
   print(plot)
 }
-dev.off()
 
 #plot the acf functions
-pdf(file = "/dss/dsshome1/03/ga27hec2/NetworkSimulationAndComparison/acf_plots.pdf")
     for(i in 1:dim){
       name <- paste0("X_", i, "_simulations")
       simulated_data <- get(name)
       par(mfrow=c(2,2))
       for (j in 1:4)  {
-        acf(simulated_data[j, ], main = paste0("X_", i, " chain ",j))
+        acf(simulated_data[j, -c(1:burn_in)], main = paste0("X_", i, " chain ",j))
       }
       par(mfrow=c(1,1)) #restore default
     }
-dev.off()
 
-
-####gelman-rubin statistics
-#calculate the test statistic (mean)
-#test it here for X_1
-phi <- t(apply(X_1_simulations, 1, cumsum))
-for (i in 1:nrow(phi)){
-phi[i,] <- phi[i,] / (1:ncol(phi))
-}
-
-Gelman.Rubin <- function(phi) {
-# phi[i,j] is the statistic phi(X[i,1:j])
-# for i-th chain and up to j-th iteration
-phi <- as.matrix(phi)
-n <- ncol(phi) # this is the number of iterations
-m <- nrow(phi) # this is the number of chains
-B <- n * var(rowMeans(phi)) #between variance estimate
-W <- 0 #Initialize W, the within variance estimate
-# Calculate row-wise means
-mean_phi_i <- rowMeans(phi)
-# Calculate the sum of squared differences
-for (i in 1:m) {
-  for (j in 1:n) {
-    W <- W + (phi[i, j] - mean_phi_i[i])^2
-  }
-}
-
-# Divide by (m * (n - 1))
-W <- W / (m * (n - 1))
-v_hat <- W*(n-1)/n + (B/n) #upper variance estimate
-R_hat <- sqrt(v_hat / W) #Gelman-Rubin statistic
-return(R_hat)
-}
-print(Gelman.Rubin(phi))
-
-
-#plot the sequence of R-hat statistics
-pdf(file = "/dss/dsshome1/03/ga27hec2/NetworkSimulationAndComparison/gelman_rubin_statistic.pdf")
-for(i in 1:dim){
-      name <- paste0("X_", i, "_simulations")
-      simulated_data <- get(name)
-      psi <- t(apply(simulated_data, 1, cumsum))
-      for (i in 1:nrow(psi)){
-      psi[i,] <- psi[i,] / (1:ncol(psi))
-      }
-      rhat <- rep(0, (iterations-burn_in))
-      for (j in 1:(iterations-burn_in)){
-      rhat[j] <- Gelman.Rubin(psi[,1:j])
-      }
-      plot(rhat, type="l", xlab="", ylab="R")
-      abline(h=1.1, lty=2)}
-dev.off()
-
-
-
-
-
-#Gelman Rubin on the mean
-
-mean_per_chain_mod <- t(apply(mean_per_chain, 1, cumsum))
-for (i in 1:nrow(mean_per_chain_mod)){
-mean_per_chain_mod[i,] <- mean_per_chain_mod[i,] / (1:ncol(mean_per_chain_mod))}
-Gelman.Rubin(mean_per_chain_mod)
-
-pdf(file = "/dss/dsshome1/03/ga27hec2/NetworkSimulationAndComparison/gelman_rubin_mean.pdf")
-#plot mean_X_new for the four chains
-par(mfrow=c(2,2))
-for (i in 1:4)
-plot(mean_per_chain[i, ], type="l",
-xlab=i, ylab=bquote(mean_X_new))
-par(mfrow=c(1,1)) #restore default
-
-#plot the sequence of R-hat statistics
-rhat <- rep(0, iterations-burn_in)
-for (j in 1:(iterations-burn_in)){
-
-rhat[j] <- Gelman.Rubin(mean_per_chain_mod[,1:j])
-}
-plot(rhat, type="l", xlab="", ylab="R hat")
-abline(h=1.1, lty=2)
-dev.off()
-
-
-###R hat with coda
-
-library(coda)
-#this needs to be done for every variable
-for (i in 1:dim){
-      name <- paste0("X_", i, "_simulations")
-      simulated_data <- get(name)
-      mcmc.list <- mcmc.list(as.mcmc(simulated_data[1,]), as.mcmc(simulated_data[2,]), as.mcmc(simulated_data[3,]) ,as.mcmc(simulated_data[4,]))
-      new_name <- paste0("mcmc.list.X", i)
-      assign(new_name, mcmc.list)
-}
-mcmc.list.X1 <- mcmc.list(as.mcmc(X_1_simulations[1,]), as.mcmc(X_1_simulations[2,]), as.mcmc(X_1_simulations[3,]) ,as.mcmc(X_1_simulations[4,]))
-gelman.diag(mcmc.list.X1)
-
-
-
-pdf(file = "/dss/dsshome1/03/ga27hec2/NetworkSimulationAndComparison/gelman_rubin_coda.pdf")
+#plot the r hat diagnostics
 for(i in 1:dim){
   mcmc.list <- paste0("mcmc.list.X", i)
   mcmc.list <- get(mcmc.list)
-  gelman.plot(mcmc.list)
+  gelman.plot(mcmc.list) #, transform = TRUE
 }
+
 dev.off()
